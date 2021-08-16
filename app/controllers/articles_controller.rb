2c1ebcac1e93d3838203c684cfab55509b90160e
TonyBHerrera <tonybherrera@yahoo.com>
2021-08-16 11:10:37 -0600

    adding upon completion or failure to post messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,8 +8,15 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
     def new 
+        @article = Article.new
     end
     def create 
-        render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was create successfully."
+            redirect_to @article
+        else 
+            render 'new'
+        end
     end
 end
