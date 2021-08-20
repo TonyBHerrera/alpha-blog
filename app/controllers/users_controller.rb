8a9faf85f58f4ce88b9e02be86414fbb5b822968
TonyBHerrera <tonybherrera@yahoo.com>
2021-08-20 09:32:52 -0600

    add edit templete and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,24 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def edit 
+        @user = User.find(params[:id])
+    end 
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Your account info was successfully updated."
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+
+    end
+
     def create
-        @user =  User.new(user_params)
+        @user = User.new(user_params)
         if @user.save
             flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have sucessfully signed up"
             redirect_to articles_path
@@ -15,5 +31,6 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
+    
 
 end
